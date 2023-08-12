a16e76d854f387f67d62e8ef08f7cb5d0949fc0e
Mehemmed77 <mehemmedkazimzade54@gmail.com>
2023-08-12 15:59:00 +0400

    new-version-1

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,11 +1,39 @@
+const container = document.querySelector("#container");
+
 let url = "../db/scoareBoard.json"
 function getData(){
     fetch(url)
     .then((res)=> res.json())
     .then((data)=> {
         let {scoreBoard, onlineTutorials} = data;
-        console.log(scoreBoard)
-        console.log(data);
+
+        scoreBoard.forEach(element => {
+            container.insertAdjacentHTML("beforeend",`
+                <div class="exam">
+                <div><img class="card-image" src="./assets/image/Tiny students learning online lesson via laptop 1.svg"
+                        alt=""></div>
+                <div class="card-info">
+                    <div class="exam-info">
+                        <div class="question-number">25 sual</div>
+                        <div class="teacher-name">${element.fullName}</div>
+                    </div>
+                    <div class="exam-name">9-cu sinif buraxılış</div>
+                    <small class="question-data">23/24</small>
+                    <div class="input">
+                        <div>
+                            <span id="rangeValue">0</span>
+                            <input class="range" type="range" name="" value="0" min="0" max="100"
+                            >
+                        </div>
+                    </div>
+                    <div class="range"></div>
+                </div>
+                <div class="card-buttons">
+                    <button class="more-details">Daha ətraflı</button>
+                </div>
+            </div>            
+            `)
+        });
     })
 }
 
--- js/script.js
+++ js/script.js
@@ -1,57 +1,38 @@
+const root = document.querySelector(':root');
+
+
+
 let icon=document.querySelector('.arrow');
 
 icon.addEventListener('click', () =>{
     icon.classList.toggle('active');
 });
 
-// let listItems = document.querySelectorAll('.sidebar-list-item');
-// listItems.forEach((list) => {
-//     list.addEventListener('click', (e) =>{
-//         listItems.forEach(item=> item.classList.remove('active'));
-//         e.target.classList.add('active');
-//     })
-// })
-
 let inputsdiv = document.querySelectorAll('.input div');
 inputsdiv.forEach((div) => {
     let input = div.querySelector('input');
     let span = div.querySelector('#rangeValue');
     input.addEventListener('change', (e) =>{
-        span.innerHTML = e.target.value;
+    span.innerHTML = e.target.value;
     })
 });
 
-const sidebar = document.querySelector('.sidebar');
-const sidebarTitle=document.querySelector('#span_sidebar');
-const toggle = document.getElementById("toggle");
-const header = document.querySelector("header")
-const main = document.querySelector(".main");
-const infostudent = document.querySelector(".about_student")
-const prize= document.querySelector(".prize_student");
-const examcard = document.querySelector(".exam");
-const spanheader= document.querySelector("#span_header")
-const mail= document.querySelector(".mail");
-const exam= document.querySelector("#exam");
-
-const list=document.getElementById("list-item");
-const listItems =list.children[1];
-
 toggle.onclick = function(){
-sidebarTitle.classList.toggle('active');
-toggle.classList.toggle("active");
-sidebar.classList.toggle("active");
-header.classList.toggle("active");
-main.classList.toggle("active");
-infostudent.classList.toggle("active");
-prize.classList.toggle("active");
-spanheader.classList.toggle("active");
-mail.classList.toggle("active");
-exam.classList.toggle("active");
+    toggle.classList.toggle("active");
+    
+    if (toggle.classList.contains("active")){
+        root.style.setProperty("--text-color","white");
+        root.style.setProperty("--title-color","white");
+        root.style.setProperty("--data-color","white");
+        root.style.setProperty("--main-bg","#3B323C");
+        root.style.setProperty("--container-bg","#2B2A2A");
+    }
 
-examcard.classList.toggle("active");
-
-
-
-    listItems.classList.toggle("active");
-
+    else{
+        root.style.setProperty("--text-color","black");
+        root.style.setProperty("--title-color","#675AF0");
+        root.style.setProperty("--data-color","red");
+        root.style.setProperty("--main-bg","#F6F6F6");
+        root.style.setProperty("--container-bg","white");
+    }
 }
